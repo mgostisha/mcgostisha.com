c36f3e10b1655758f671097095b02f4a0ef26da8
Martin Gostisha <martin@flipgrid.com>
2019-09-18 22:10:57 -0500

    Make footer i guess

--- src/components/layout.js
+++ src/components/layout.js
@@ -60,10 +60,43 @@ const Layout = ({ location, title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
-        © Martin Gostisha {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <footer
+        style={{ display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-between', alignItems: 'flex-end' }}
+      >
+        <div
+          style={{ display: 'flex', flexFlow: 'column nowrap', justifyContent: 'flex-end', alignItems: 'flex-start' }}
+        >
+          <div>© Martin Gostisha {new Date().getFullYear()}</div>
+          <div>
+            <a
+              href="https://twitter.com/mg0stisha"
+              target="_blank"
+              rel="noreferrer noopener"
+              style={{ marginRight: '8px' }}
+            >
+              Twitter
+            </a>
+            <a
+              href="https://instagram.com/mg0stisha"
+              target="_blank"
+              rel="noreferrer noopener"
+              style={{ marginRight: '8px' }}
+            >
+              Instagram
+            </a>
+            <a
+              href="https://github.com/mg0stisha"
+              target="_blank"
+              rel="noreferrer noopener"
+              style={{ marginRight: '8px' }}
+            >
+              Github
+            </a>
+          </div>
+        </div>
+        <div style={{ marginTop: '8px' }}>
+          Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </div>
       </footer>
     </div>
   );
