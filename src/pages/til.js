36e34c27a2f932673780bc53f73fb8a5e94fd167
Martin Gostisha <martingostisha@gmail.com>
2019-09-20 08:59:22 -0500

    Add TIL, fix 404 page

--- src/pages/til.js
+++ src/pages/til.js
@@ -4,19 +4,18 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
-const NotFoundPage = ({ data, location }) => {
+const TIL = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <SEO title="TIL" />
+      This is where some TILs would go!
     </Layout>
   );
 };
 
-export default NotFoundPage;
+export default TIL;
 
 export const pageQuery = graphql`
   query {
