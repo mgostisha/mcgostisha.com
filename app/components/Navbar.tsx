bdceab4175aaca32e54f5806169340e51f67eb3d
Martin Gostisha <martingostisha@gmail.com>
2022-02-06 15:42:46 -0600

    a11y, fix themes, styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,17 @@
+import { useEffect, useState } from 'react';
 import { Link } from '~/core';
 
 export default function Navbar() {
-  const prefersDarkMode = true;
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+  }, []);
+
   return (
     <nav className="navbar">
-      <Link to="/">
-        <img src={prefersDarkMode ? '/images/site-logo-light.svg' : '/images/site-logo.svg'} alt="" className="navbar__logo" />
+      <Link to="/" aria-label="Back to home page">
+        <img src={theme === 'light' ? '/images/site-logo.svg' : '/images/site-logo-light.svg'} alt="" className="navbar__logo" />
       </Link>
     </nav>
   );
