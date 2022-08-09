a77d3e394aff89f4eb8aa2a5ebffeb3e996e1737
Martin Gostisha <martingostisha@Martins-Air.attlocal.net>
2022-08-09 12:16:02 -0500

    Change to flip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,7 +20,7 @@ export default function Index() {
   return (
     <div className="index grid">
       <h1>I'm <Outline>Marty</Outline>, an engineer(ing manager).</h1>
-      <p>I lead the <Outline>web product team</Outline> for <Link external to="https://info.flipgrid.com">Flipgrid</Link> at <Link external to="https://careers.microsoft.com/us/en/search-results?keywords=Flipgrid">Microsoft</Link>.</p>
+      <p>I lead the <Outline>web product team</Outline> for <Link external to="https://info.flip.com">Flip</Link> at <Link external to="https://careers.microsoft.com/us/en/search-results?keywords=Flipgrid">Microsoft</Link>.</p>
       <p>I'm <Outline>writing</Outline> on my <Link to="blog">blog</Link>.</p>
       <p>Sometimes I <Link external to="https://twitter.com/mg0stisha">tweet</Link>.</p>
       <p>I post snippets about what I'm <Outline>learning</Outline> on <Link to="til">TIL</Link>.</p>
