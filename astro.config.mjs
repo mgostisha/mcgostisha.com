9f87b384dcce2cfc01c6ee387e1c31e4d74e48f7
Martin Gostisha <martingostisha@gmail.com>
2022-09-06 17:54:03 -0500

    mdx, sorting, first real blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        shikiConfig: {
-            theme: 'vitesse-dark'
-        }
+  markdown: {
+    shikiConfig: {
+      theme: 'vitesse-dark'
     }
+  },
+  integrations: [mdx()]
 });
