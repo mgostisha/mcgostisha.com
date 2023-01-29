e8059965a7a4cf1189bc53965cc736d2c1c1f70e
Martin Gostisha <martingostisha@gmail.com>
2023-01-29 11:32:39 -0600

    footnotes

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -1,4 +1,4 @@
-export function formatDateForHeader(dateString?: string): string {
+export function formatDateForHeader(dateString: string | undefined): string | undefined {
     if (!dateString) return dateString;
     return new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(dateString));
 }
--- src/pages/blog/another-one.md
+++
@@ -1,8 +0,0 @@
----
-layout: ../../layouts/Post.astro
-title: Another BLOG POST!
-createdAt: '2022-09-03T16:55:46.436Z'
-updatedAt: null
-draft: true
----
-Hello! This is a blog post. I'm pasting my weird-ass concept from notion in here.
